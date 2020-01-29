3b90c923bb2c7fd388bbb8e1ad2fb3432d256374
Mateusz Broniszewski <mateusz.broniszewski.programista@gmail.com>
2020-01-29 18:34:42 +0100

    Added main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,67 @@
 #include <iostream>
 
+#include "PersonalBudget.h"
+
+
 using namespace std;
 
 int main()
 {
+    PersonalBudget personalBudget("users.xml", "incomes.xml", "expenses.xml");
+    char choice;
+
+    while(true)
+    {
+        if(personalBudget.getIdOfLoggedInUser() == 0)
+        {
+            choice = personalBudget.chooseOptionFromMainMenu();
+
+            switch(choice)
+            {
+            case '1':
+                personalBudget.userRegistration();
+                break;
+            case '2':
+                personalBudget.userLogin();
+                break;
+            case '9':
+                exit(0);
+            default:
+                cout << endl << "Nie ma takiej opcji w menu." << endl;
+                system("pause");
+                break;
+            }
+        }else
+        {
+            choice = personalBudget.chooseOptionFromUserMenu();
+
+            switch (choice)
+            {
+            case '1':
+                personalBudget.addIncome();
+                break;
+            case '2':
+                personalBudget.addExpense();
+                break;
+            case '3':
+                personalBudget.balanceFromCurrentMonth();
+                break;
+            case '4':
+                personalBudget.balanceFromPreviousMonth();
+                break;
+            case '5':
+                personalBudget.balanceFromSelectedPeriod();
+                break;
+            case '6':
+                personalBudget.changingUserPasword();
+                break;
+            case '7':
+                personalBudget.userLogout();
+                break;
+            }
+        }
+    }
+//    personalBudget.viewAllUsers();
 
     return 0;
 }
