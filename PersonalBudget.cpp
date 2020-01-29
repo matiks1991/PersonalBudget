1e83f439ab3d64fd75675383ef70f9bd267236d3
Mateusz Broniszewski <mateusz.broniszewski.programista@gmail.com>
2020-01-29 16:51:06 +0100

    Added PersonalBudget - WIP

---
+++ PersonalBudget.cpp
@@ -0,0 +1,144 @@
+#include "PersonalBudget.h"
+
+char PersonalBudget::chooseOptionFromMainMenu()
+{
+    char choice;
+
+    system("cls");
+    cout << "    >>> MENU  GLOWNE <<<" << endl;
+    cout << "---------------------------" << endl;
+    cout << "1. Rejestracja" << endl;
+    cout << "2. Logowanie" << endl;
+    cout << "9. Koniec programu" << endl;
+    cout << "---------------------------" << endl;
+    cout << "Twoj wybor: ";
+
+    choice = AuxiliaryMethods::loadCharacter();
+
+    return choice;
+}
+
+char PersonalBudget::chooseOptionFromUserMenu()
+{
+    char choice;
+
+    system("cls");
+    cout << " >>> MENU UZYTKOWNIKA <<<" << endl;
+    cout << "---------------------------" << endl;
+    cout << "1. Dodaj przychod" << endl;
+    cout << "2. Dodaj wydatek" << endl;
+    cout << "3. Bilans z biezacego miesiaca" << endl;
+    cout << "4. Bilans z poprzedniego miesiaca" << endl;
+    cout << "5. Bilans z wybranego okresu" << endl;
+    cout << "---------------------------" << endl;
+    cout << "6. Zmien haslo" << endl;
+    cout << "7. Wyloguj sie" << endl;
+    cout << "---------------------------" << endl;
+    cout << "Twoj wybor: ";
+
+    choice = AuxiliaryMethods::loadCharacter();
+
+    return choice;
+}
+
+void PersonalBudget::userRegistration()
+{
+    managerUser.userRegistration();
+}
+
+void PersonalBudget::viewAllUsers()
+{
+    managerUser.viewAllUsers();
+}
+
+void PersonalBudget::userLogin()
+{
+    managerUser.userLogin();
+    if(managerUser.checkIfUserIsLoggedIn())
+    {
+//        managerBudget = new ManagerBudget(FILE_NAME_WITH_INCOMES, FILE_NAME_WITH_EXPANSES, managerUser.getIdOfLoggedInUser());
+    }
+}
+
+void PersonalBudget::changingUserPasword()
+{
+    managerUser.changingUserPasword();
+}
+
+void PersonalBudget::userLogout()
+{
+    managerUser.userLogout();
+//    delete managerBudget;
+//    managerBudget = NULL;
+}
+
+void PersonalBudget::addIncome()
+{
+    if(managerUser.checkIfUserIsLoggedIn())
+    {
+//        managerBudget->addIncome();
+    }
+    else
+    {
+        cout << "Aby dodac adresata, nalezy najpierw sie zalogowac" << endl;
+        system("pause");
+    }
+}
+
+void PersonalBudget::addExpense()
+{
+    if(managerUser.checkIfUserIsLoggedIn())
+    {
+//        managerBudget->addExpense();
+    }
+    else
+    {
+        cout << "Aby dodac adresata, nalezy najpierw sie zalogowac" << endl;
+        system("pause");
+    }
+}
+
+void PersonalBudget::balanceFromCurrentMonth()
+{
+    if(managerUser.checkIfUserIsLoggedIn())
+    {
+//        managerBudget->balanceFromCurrentMonth();
+    }
+    else
+    {
+        cout << "Aby wyswietlic wszystkich adresatow, nalezy najpierw sie zalogowac" << endl;
+        system("pause");
+    }
+}
+
+void PersonalBudget::balanceFromPreviousMonth()
+{
+    if(managerUser.checkIfUserIsLoggedIn())
+    {
+//        managerBudget->balanceFromPreviousMonth();
+    }
+    else
+    {
+        cout << "Aby wyswietlic wszystkich adresatow, nalezy najpierw sie zalogowac" << endl;
+        system("pause");
+    }
+}
+
+void PersonalBudget::balanceFromSelectedPeriod()
+{
+    if(managerUser.checkIfUserIsLoggedIn())
+    {
+//        managerBudget->balanceFromSelectedPeriod();
+    }
+    else
+    {
+        cout << "Aby wyswietlic wszystkich adresatow, nalezy najpierw sie zalogowac" << endl;
+        system("pause");
+    }
+}
+
+int PersonalBudget::getIdOfLoggedInUser()
+{
+    managerUser.getIdOfLoggedInUser();
+}
+
---
+++ PersonalBudget.h
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include "ManagerUser.h"
+//#include "ManagerBudget.h"
+
+using namespace std;
+
+class PersonalBudget
+{
+    ManagerUser managerUser;
+//    ManagerBudget *managerBudget;
+    const string FILE_NAME_WITH_INCOMES;
+    const string FILE_NAME_WITH_EXPANSES;
+
+public:
+    PersonalBudget(string fileNameWithUsers, string fileNameWithIncomes, string fileNameWithExpenses)
+    :managerUser(fileNameWithUsers), FILE_NAME_WITH_INCOMES(fileNameWithIncomes), FILE_NAME_WITH_EXPANSES(fileNameWithExpenses)
+    {
+//        managerBudget = NULL;
+    };
+    ~PersonalBudget()
+    {
+//        delete managerBudget;
+//        managerBudget = NULL;
+    }
+
+    void userRegistration();
+    void viewAllUsers();
+    void userLogin();
+    void changingUserPasword();
+    void userLogout();
+    void addIncome();
+    void addExpense();
+    void balanceFromCurrentMonth();
+    void balanceFromPreviousMonth();
+    void balanceFromSelectedPeriod();
+    int getIdOfLoggedInUser();
+    char chooseOptionFromMainMenu();
+    char chooseOptionFromUserMenu();
+};
