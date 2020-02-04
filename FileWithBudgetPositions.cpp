785eab9a1732d9831d33cb5adb77b8b505f514db
Mateusz Broniszewski <mateusz.broniszewski.programista@gmail.com>
2020-02-04 18:42:47 +0100

    Added FileWithBudgetPositions

---
+++ FileWithBudgetPositions.cpp
@@ -0,0 +1,111 @@
+#include "FileWithBudgetPositions.h"
+
+using namespace std;
+
+bool FileWithBudgetPositions::addBudgetPositions(BudgetPosition budgetPosition)
+{
+    CMarkup xmlFile;
+    bool fileExists, fileSaved;
+
+    fileExists = xmlFile.Load(getFileName().c_str());
+
+     if (!fileExists)
+    {
+        xmlFile.SetDoc("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n");
+        xmlFile.AddElem("BudgetPositions");
+    }
+
+    xmlFile.FindElem();
+    xmlFile.IntoElem();
+    xmlFile.AddElem("BudgetPosition");
+    xmlFile.IntoElem();
+    xmlFile.AddElem("Id", budgetPosition.getId());
+    xmlFile.AddElem("UserId", budgetPosition.getUserId());
+    xmlFile.AddElem("Date", budgetPosition.getDate());
+    xmlFile.AddElem("Item", budgetPosition.getItem());
+    xmlFile.AddElem("Amount", AuxiliaryMethods::conversionDoubleToString(budgetPosition.getAmount()));
+
+    fileSaved = xmlFile.Save(getFileName().c_str());
+
+    if(fileSaved)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+vector<BudgetPosition> FileWithBudgetPositions::getBudgetPositionsOfLoggedInUserFromFile(int idLoggedInUser)
+{
+    CMarkup xmlFile;
+    bool fileExist;
+    vector<BudgetPosition> budgetPositions;
+    BudgetPosition budgetPosition;
+
+    fileExist = xmlFile.Load(getFileName().c_str());
+
+    if(!fileExist)
+        return budgetPositions;
+
+    xmlFile.FindElem();
+    xmlFile.IntoElem();
+
+    while(xmlFile.FindElem("BudgetPosition"))
+    {
+        xmlFile.IntoElem();
+        xmlFile.FindElem("Id");
+        budgetPosition.setId(AuxiliaryMethods::conversionStringToInt(MCD_2PCSZ(xmlFile.GetData())));
+        xmlFile.FindElem("UserId");
+        budgetPosition.setUserId(AuxiliaryMethods::conversionStringToInt(MCD_2PCSZ(xmlFile.GetData())));
+
+        if(budgetPosition.getUserId() != idLoggedInUser)
+        {
+            xmlFile.OutOfElem();
+            continue;
+        }
+
+        xmlFile.FindElem("Date");
+        budgetPosition.setDate(xmlFile.GetData());
+        xmlFile.FindElem("Item");
+        budgetPosition.setItem(xmlFile.GetData());
+        xmlFile.FindElem("Amount");
+        budgetPosition.setAmount(atof(MCD_2PCSZ(xmlFile.GetData())));
+        xmlFile.OutOfElem();
+
+        budgetPositions.push_back(budgetPosition);
+    }
+
+    return budgetPositions;
+}
+
+int FileWithBudgetPositions::getLastBudgetPositionIdFromFile()
+{
+    CMarkup xmlFile;
+    bool fileExist;
+    int idOfLastBudgetPosition = 0;
+
+    fileExist = xmlFile.Load(getFileName().c_str());
+
+    if(!fileExist)
+    {
+        return 5;
+    }
+
+    xmlFile.FindElem();
+    xmlFile.IntoElem();
+
+    while(xmlFile.FindElem("BudgetPosition"))
+    {
+        xmlFile.IntoElem();
+        xmlFile.FindElem("Id");
+        idOfLastBudgetPosition = AuxiliaryMethods::conversionStringToInt(MCD_2PCSZ(xmlFile.GetData()));
+        xmlFile.FindElem("UserId");
+        xmlFile.FindElem("Date");
+        xmlFile.FindElem("Item");
+        xmlFile.FindElem("Amount");
+        xmlFile.OutOfElem();
+    }
+
+    return idOfLastBudgetPosition;
+}
+
---
+++ FileWithBudgetPositions.h
@@ -0,0 +1,26 @@
+#ifndef FILEWITHBUDGETPOSITIONS_H
+#define FILEWITHBUDGETPOSITIONS_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+#include <string.h>
+
+#include "BudgetPosition.h"
+#include "AuxiliaryMethods.h"
+#include "TextFile.h"
+#include "Markup.h"
+
+using namespace std;
+
+class FileWithBudgetPositions : public TextFile
+{
+    public:
+    FileWithBudgetPositions(string FILENAMEWITHBUDGETPOSITIONS) : TextFile(FILENAMEWITHBUDGETPOSITIONS);
+    bool addBudgetPositions(BudgetPosition budgetPosition);
+    vector<BudgetPosition> getBudgetPositionsOfLoggedInUserFromFile(int idLoggedInUser);
+    int getLastBudgetPositionIdFromFile();
+};
+
+#endif
